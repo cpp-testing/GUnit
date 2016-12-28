483e7c9b55a102d2e4d75e01c13b49e780182344
Kris Jusiak <krzysztof@jusiak.net>
2016-12-28 15:02:07 +0000

    [Benchmark] mock -> GMock

--- benchmark/gmock.vptr/test.cpp
+++ benchmark/gmock.vptr/test.cpp
@@ -10,9 +10,9 @@
 
 TEST(benchmark, test1) {
   using namespace testing;
-  mock<interface1> m1;
-  mock<interface2> m2;
-  mock<interface3> m3;
+  GMock<interface1> m1;
+  GMock<interface2> m2;
+  GMock<interface3> m3;
 
   EXPECT_CALL(m1, (f1)(42)).WillOnce(Return(true));
   EXPECT_CALL(m2, (f2_1)()).Times(1);
@@ -25,9 +25,9 @@ TEST(benchmark, test1) {
 
 TEST(benchmark, test2) {
   using namespace testing;
-  mock<interface1> m1;
-  mock<interface2> m2;
-  mock<interface3> m3;
+  GMock<interface1> m1;
+  GMock<interface2> m2;
+  GMock<interface3> m3;
 
   EXPECT_CALL(m1, (f1)(42)).WillOnce(Return(false));
   EXPECT_CALL(m2, (f2_2)()).Times(1);
