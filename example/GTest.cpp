76ad06f88dc2e618154965f64ccc16e0cbe4402f
Krzysztof Jusiak <krzysztof@jusiak.net>
2017-01-16 22:15:57 +0000

    [GUnit] GUnit header

--- example/GTest.cpp
+++ example/GTest.cpp
@@ -5,7 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#include "GTest.h"
+#include "GUnit/GTest.h"
 
 struct interface {
   virtual ~interface() = default;
---
+++ include/GUnit.h
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2016-2017 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+#include "GUnit/GMock.h"
+#include "GUnit/GTest.h"
