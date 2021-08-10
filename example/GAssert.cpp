7b9358c503926407852363a2ebe12d1aebbaca83
Kris Jusiak <kris@jusiak.net>
2021-08-10 01:30:12 +0000

    :arrow_up: Update gtest to v1.10.x version

--- example/GAssert.cpp
+++ example/GAssert.cpp
@@ -5,9 +5,10 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
+#include "GUnit/GTest.h"
 #include "GUnit/GAssert.h"
 
-int main() {
+GTEST("Assert") {
   const auto b = true;
   const auto i = 42;
 
